76cdf2c6b0702969b084039e1f117962a866651d
Jennifer Luong <luong.jenniferh@gmail.com>
2018-09-04 17:38:19 -0700

    Set up class packages

---
+++ src/api/Main.java
@@ -0,0 +1,10 @@
+package api;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.print("Groovy Tunes");
+	}
+
+}
---
+++ src/api/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author jenniferluong
+ *
+ */
+package api;

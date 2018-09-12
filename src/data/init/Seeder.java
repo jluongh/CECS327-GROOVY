1775244e5057b2746cf7b98c422eaa099e0cf1f2
Trisha Echual <piechual@gmail.com>
2018-09-12 00:55:30 -0700

    Expand store for search implementation
    Expand store to test search functionality on store of user info.

--- src/data/init/Seeder.java
+++ src/data/init/Seeder.java
@@ -4,29 +4,52 @@ import java.io.FileOutputStream;
 import java.util.*;
 
 import com.google.gson.Gson;
-import data.models.User;
+import data.models.*;
 
 public class Seeder {
 	private static final String UNIX_PATH = "src/data/"; 
 	
 	public static void main(String[] args) {
-		User user1 = new User("user1", "cecs327", null);
-		User user2 = new User("user2", "cecs327", null);
+		
+		// Create Date object
+		Calendar today = Calendar.getInstance();
+		today.clear(Calendar.HOUR); 
+		today.clear(Calendar.MINUTE); 
+		today.clear(Calendar.SECOND);
+		Date todayDate = today.getTime();
+		
+		// Create playlists
+		Playlist playlist1 = new Playlist(1, "80s love songs", Collections.emptyList(), 16, "1 hr 2 min", todayDate);
+		Playlist playlist2 = new Playlist(2, "Bangerz", Collections.emptyList(), 11, "45 min", todayDate);
+		
+		List<Playlist> playlists = new ArrayList<Playlist>();
+		playlists.add(playlist1);
+		playlists.add(playlist2);
+		
+		// Create users
+		User user1 = new User("user1", "cecs327", playlists);
+		User user2 = new User("user2", "cecs327", playlists);
+		User user3 = new User("user3", "cecs327", Collections.emptyList());
 		
 		List<User> users = new ArrayList<User>();
 		users.add(user1);
 		users.add(user2);
+		users.add(user3);
 		
-		Gson gson = new Gson();
+		// Create store
+		Store store = new Store(users);
+		
 		
 		try {
-			
+			Gson gson = new Gson();
+		
 			FileOutputStream out = new FileOutputStream(UNIX_PATH + "store.txt");
-			out.write(gson.toJson(users).getBytes());
+			out.write(gson.toJson(store).getBytes());
 			out.close();
-			
+		
 		} catch (Exception e) {
 			System.out.println(e);
 		}
+			
 	}
 }

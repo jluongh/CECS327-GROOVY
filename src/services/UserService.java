446710f98369cc24cf2ac786e932b6dd9b851484
Unknown <luong.jenniferh@gmail.com>
2018-09-13 11:04:36 -0700

    Created a playlist service

--- src/services/UserService.java
+++ src/services/UserService.java
@@ -7,6 +7,8 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+
 import data.models.*;
 
 /**
@@ -15,13 +17,42 @@ import data.models.*;
  */
 public class UserService {
 
-	private String fPath = "./src/data/store.json";
+	private String fPath = "./src/data/user.json";
 	
 	/**
 	 * 
 	 */
 	public UserService() {
 		//
+	}
+	
+	public User getUser(String username) {
+		// Create file object from store
+		File file = new File(fPath);
+		
+		String json = "";
+		
+		// Read each character token in file
+		try (FileInputStream in = new FileInputStream(file)) {
+			
+			int w;
+			while ((w = in.read()) != -1) {
+				json = json + (char) w;
+				
+			}
+			
+			// Identify token type for deserialization
+			Gson gson = new Gson();
+			Type listType = new TypeToken<List<User>>() {}.getType();
+
+			List<User> yourList = new Gson().fromJson(json, listType);
+
+			return yourList.stream().filter(u -> u.getUsername().equals(username)).findFirst().get();
+		} catch(Exception e) {
+			System.out.print(e);
+			return null;
+			
+		}		
 	}
 	
 	/**
@@ -47,9 +78,11 @@ public class UserService {
 			
 			// Identify token type for deserialization
 			Gson gson = new Gson();
-			Store store = gson.fromJson(json, new TypeToken<Store>() {}.getType());
+			Type listType = new TypeToken<List<User>>() {}.getType();
+
+			List<User> yourList = new Gson().fromJson(json, listType);
+			return yourList;
 			
-			return store.getUsers();
 			
 		} catch(Exception e) {
 			

b7a8f3af4f12fb7fd6a5b5c5e6a1eaecaec3988a
Unknown <luong.jenniferh@gmail.com>
2018-10-04 11:32:56 -0700

    Client/Service UDP

---
+++ src/api/Server.java
@@ -0,0 +1,15 @@
+package api;
+
+import java.io.IOException;
+
+import api.threads.PlaylistThread;
+
+public class Server {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		new PlaylistThread().start();
+
+	}
+
+}
--- src/application/Client.java
+++ src/application/Client.java
@@ -1,52 +1,35 @@
 package application;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.net.Socket;
-import java.util.Scanner;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
-public class Client 
-{
-	private final PrintStream printStream;
+public class Client {
 	
-	@SuppressWarnings("CallToThreadStartDuringObjectConstruction")
-	public Client(String ip,int port)throws IOException
-	{
-		Socket s = new Socket(ip,port);
-		
-		new Thread(() ->  {
-			try {
-				BufferedReader reader = new BufferedReader(new InputStreamReader(s.getInputStream()));
-				String line;
-				
-				while((line = reader.readLine())!=null) {
-					System.out.print("Server response : " + line);
-					System.out.print("\nCommands [add, sub, mul, div, mod, exit] : ");
-				}
-			} catch (IOException ex) {
-				System.err.println("\nDisconnected");
-				System.exit(0);
-			}
-		}).start();
-		
-		printStream = new PrintStream(s.getOutputStream(), true);
-	}
-	
-	public void sendMessage(String operation) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("\nEnter 1st number : ");
-        int f1 = in.nextInt();
-
-        System.out.print("Enter 2nd number : ");
-        int s1 = in.nextInt();
-
-        printStream.println(operation + ":" + f1 + ":" + s1);
-
+	public static void main(String[] args) throws IOException {
+		 
+        if (args.length != 1) {
+             System.out.println("Usage: java QuoteClient <hostname>");
+             return;
+        }
+        
+            // get a datagram socket
+        DatagramSocket socket = new DatagramSocket();
+        
+            // send request
+        byte[] buf = new byte[4000];
+        InetAddress address = InetAddress.getByName(args[0]);
+        DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 4445);
+        socket.send(packet);
+     
+            // get response
+        packet = new DatagramPacket(buf, buf.length);
+        socket.receive(packet);
+ 
+        // display response
+        String received = new String(packet.getData(), 0, packet.getLength());
+        System.out.println(received);
+     
+        socket.close();
     }
-
-	public void connect()
-	{
-		
-	}
+	
 }

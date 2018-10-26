2f5b90013f45d32cada3217268d5a365338435f5
Trisha Echual <piechual@gmail.com>
2018-10-25 22:39:46 -0700

    fix guid error I created

--- src/data/index/File.java
+++ src/data/index/File.java
@@ -13,9 +13,9 @@ public class File {
 		a.name = "song.txt";
 		a.size = "B";
 		Chunk a1 = new Chunk();
-		a1.text = "Everybody Wants To Rule The World;1;Tears For Fears;Songs From The Big Chair;4.13";
+		a1.guid = "Everybody Wants To Rule The World;1;Tears For Fears;Songs From The Big Chair;4.13";
 		Chunk a2 = new Chunk();
-		a2.text = "September;2;Earth, Wind, \\u0026 Fire;Greatest Hits;3.35";
+		a2.guid = "September;2;Earth, Wind, \\u0026 Fire;Greatest Hits;3.35";
 
 		a.chunks.add(a1);
 		a.chunks.add(a2);

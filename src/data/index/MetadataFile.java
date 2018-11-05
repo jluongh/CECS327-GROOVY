59d856bc59120fb31ae917bf7acdc58e5374fb6f
Trisha Echual <piechual@gmail.com>
2018-11-05 02:10:22 -0800

    Chunk and MDF models

--- src/data/index/MetadataFile.java
+++ src/data/index/MetadataFile.java
@@ -1,39 +1,39 @@
 package data.index;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import services.HashService;
+import java.util.*;
 
 public class MetadataFile {
+
 	private String name;
-	private String size;
+	private int size;
 	private List<Chunk> chunks = new ArrayList<Chunk>();
-	private HashService hs = new HashService();
+	
+	public MetadataFile() {
+		//
+	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getSize() {
+
+	public int getSize() {
 		return size;
 	}
-	public void setSize(String size) {
+
+	public void setSize(int size) {
 		this.size = size;
 	}
+
 	public List<Chunk> getChunks() {
 		return chunks;
 	}
+
 	public void setChunks(List<Chunk> chunks) {
 		this.chunks = chunks;
 	}
-	public void append(String content)
-	{
-		String guid = hs.sha1(content);
-		Chunk c = new Chunk();
-		c.setGuid(guid);
-		chunks.add(c);
-		//find peer and put guid and content
-	}
+	
 }

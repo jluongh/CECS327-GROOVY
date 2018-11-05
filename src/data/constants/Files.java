d0686a514bf39e248aaecdbaf5a5b21aa2b199e6
Trisha Echual <piechual@gmail.com>
2018-11-05 02:20:41 -0800

    Add chunk hashes to indices

--- src/data/constants/Files.java
+++ src/data/constants/Files.java
@@ -8,4 +8,5 @@ public class Files {
 	public final static String USERS = "./src/data/users.txt";
 	public final static String SONGS = "./src/data/song.txt";
 
+	public final static char[] limits = {'a','f','g','m','n','z'};
 }

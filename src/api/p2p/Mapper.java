8bc34c8485dfde37af21177edf29e696435d7fd1
Caitlin Rubia <caitlinrubia@gmail.com>
2018-11-29 10:57:17 -0800

    fixed Mapper

--- src/api/p2p/Mapper.java
+++ src/api/p2p/Mapper.java
@@ -4,13 +4,13 @@ import java.io.IOException;
 
 public class Mapper implements MapInterface, ReduceInterface{
 	public void map(Integer key, String value) throws IOException{
-		for (word:value) {
-			emit(md5(word), word +":"+1);
+		for (word: value) {
+			emit(md5(word), word +":"+ 1);
 		}
 	}
 	
 	public void reduce(Integer key, String values[]) throws IOException{
-	word = values[0].split(":")[0]
-	emit(key, word +":"+ len(values));
+		String word = values[0].split(":")[0];
+		emit(key, word +":"+ len(values));
 	}
 }

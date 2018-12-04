023bc9182df7ce2996dc3138e16b7e1d79686929
Unknown <luong.jenniferh@gmail.com>
2018-12-04 11:54:13 -0800

    Updated mapper

--- src/api/p2p/MetadataServiceMap.java
+++ src/api/p2p/MetadataServiceMap.java
@@ -13,190 +13,166 @@ import java.util.TreeMap;
 
 import net.tomp2p.peers.Number160;
 
-public class MetadataServiceMap extends Mapper<String, String, String, String>{
+public class MetadataServiceMap {
 
 	List<Peer> peers;
-	
-	TreeMap <Number160, List <String>> maps;
-	TreeMap <Number160, String> reduce;
+
+	TreeMap<Number160, List<String>> maps;
+	TreeMap<Number160, String> reduce;
+
+	List<Mapper> mappers;
 	
 	public MetadataServiceMap(List<Peer> peers) {
 		this.peers = peers;
 	}
-	
-	
 
-	   public void map(String key, String value, Context con) throws IOException, InterruptedException
-	    {
-	        String line = value.toString();
-	        String[] words=line.split(",");
-	        for(String word: words )
-	        {
-	            String outputKey = new String(word.toUpperCase().trim());
-	            String outputValue = new String();
-	            con.write(outputKey, outputValue);
-	        }
-	    }
-	   
-	   
-	   public void mapContext(int page, MapInterface mapper, Counter counter) {
-		   
-		   // open page file 
-		   BufferedReader reader;
-		   
-		   //file?
-		   //reader =  new BufferedReader(new FileReader());
-		   String line = reader.readLine();
-		   
-			while (line != null) {
-				
-				// read line by line execute mapper.map(key, value, counter)
-				mapper.map(key, value);
-				
-				// read next line
-				line = reader.readLine();
-			}
-			reader.close();
-		  
-		   // when its done complete file call counter increment(page n)
-		   counter.increment(page, 1);
-		   
-		   counter.hasCompleted();
-		   
-	   }
-	
-	   
-	   
-	   
-	   
-	   
-	   
+//	public void map(String key, String value, C con) throws IOException, InterruptedException {
+//		String line = value.toString();
+//		String[] words = line.split(",");
+//		for (String word : words) {
+//			String outputKey = new String(word.toUpperCase().trim());
+//			String outputValue = new String();
+//			con.write(outputKey, outputValue);
+//		}
+//	}
+
+	public void mapContext(int page, Mapper mapper, Counter counter) {
+
+		// open page file
+		BufferedReader reader;
+
+		// file?
+		// reader = new BufferedReader(new FileReader());
+		String line = reader.readLine();
+
+		while (line != null) {
+
+			// read line by line execute mapper.map(key, value, counter)
+			mapper.map(line);
+
+			// read next line
+			line = reader.readLine();
+		}
+		reader.close();
+
+		// when its done complete file call counter increment(page n)
+		counter.increment(page, 1);
+
+		counter.hasCompleted();
+
+	}
+
 	Set<Number160> set = new HashSet<>();
-	
+
 	// -------- MAPPING -------- //
 
 	// NASSER
 	// emitMap()
 	// for each file in metadata, select a peer to map
 	// Calls mapContext()
-	
-	
+
 	// mapContext()
 	// Read the inverted index file
 	// Map it
 	// increment counter
-	
-	
+
 	// -------- REDUCING -------- //
 	// Basically sorts the inverted index files
 
 	// XINY
 	// void reduce()
 	// Calls reduceContext(), completed()
-	
+
 	// CAITLIN
-	public void reduceContext(Number160 source, ReduceInterface reducer, Counter counter){
+	public void reduceContext(Number160 source, ReduceInterface reducer, Counter counter) {
 		if (source != peers.get(0).getID()) {
 			counter.add(peers.get(0).getID());
 		}
-		
-		  if (source != peers.get(0).getID()) {
-	            System.out.println("in reduceContext");
-	            successor.reduceContext(source, reducer, counter); //need function to find successor
-	        }
 
-	        Thread thread = new Thread(new Runnable() {
-	            @Override
-	            public void run() {
-	                try {
-	                    //counter.setWorkingPeer(getID());
-	                } catch (IOException e) {
-	                    e.printStackTrace();
-	                }
-	                for (Entry<String, List<String>> entry : maps.entrySet()) {
-	                    String key = entry.getKey();
-	                    List<String> values = entry.getValue();
-	                    String strings[] = new String[values.size()];
-	                    values.toArray(strings);
-	                    try {
-	                        reducer.reduce(key, values);
+		if (source != peers.get(0).getID()) {
+			System.out.println("in reduceContext");
+			successor.reduceContext(source, reducer, counter); // need function to find successor
+		}
 
-	                    } catch (IOException e) {
-	                        e.printStackTrace();
-	                    }
-	                }
+		Thread thread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					// counter.setWorkingPeer(getID());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				for (Entry<String, List<String>> entry : maps.entrySet()) {
+					String key = entry.getKey();
+					List<String> values = entry.getValue();
+					String strings[] = new String[values.size()];
+					values.toArray(strings);
+					try {
+						reducer.reduce(key, values);
 
-	                System.out.println("Finish mapReduce.");
-	      
-	            }
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
 
-	        });
-	        thread.run();
+				System.out.println("Finish mapReduce.");
 
+			}
+
+		});
+		thread.run();
 
 	}
-	
 
 	// Calls emitReduce()
-	
-	public void emitReduce(Number160 key, String value,Counter counter)
-	{
-		if(isKeyBetween(key,predecessor.getID(),successor.getID())) //need to implement get predecessor and successor
+
+	public void emitReduce(Number160 key, String value, Counter counter) {
+		if (isKeyBetween(key, predecessor.getID(), successor.getID())) // need to implement get predecessor and
+																		// successor
 		{
 			reduce.put(key, value);
 			counter.decrement();
-		}
-		else
-		{
+		} else {
 			Peer peer = this.locateSuccessor(key);
-			peer.emitReduce(key,value);
+			peer.emitReduce(key, value);
 		}
-		
+
 	}
-	
-	
-	public Boolean completed(Number160 source, Counter counter)
-	{
-		if(source!=peers.get(0).getID())
-		{
+
+	public Boolean completed(Number160 source, Counter counter) {
+		if (source != peers.get(0).getID()) {
 			counter.add(peers.get(0).getID());
 		}
-		successor.completed(source,counter);
-		//create new file stores the tree in file output in page guid 
+		successor.completed(source, counter);
+		// create new file stores the tree in file output in page guid
 		counter.increment(peers.get(0).getID(), 0);
 	}
-	
-	
-	public void runMapReduce(File file)
-	{
+
+	public void runMapReduce(File file) {
 		Counter mapCounter = new Counter();
 		Counter reduceCounter = new Counter();
 		Counter completedCounter = new Counter();
 		MapInterface mapper;
 		ReduceInterface reducer;
-		
-		while (!mapCounter.hasCompleted())
-		{
-			//map Phases
-			//for each page in metafile.file
-				mapCounter.add(page);
-				//let peer = storing pages
-				peer.mapContext(page,mapper,mapCounter);
-			//wait till
+
+		while (!mapCounter.hasCompleted()) {
+			// map Phases
+			// for each page in metafile.file
+			mapCounter.add(page);
+			// let peer = storing pages
+			peer.mapContext(page, mapper, mapCounter);
+			// wait till
 		}
-		while (!reduceCounter.hasCompleted())
-		{
-			reduceContext(guid,reducer,reduceCounter);
+		while (!reduceCounter.hasCompleted()) {
+			reduceContext(guid, reducer, reduceCounter);
 		}
-		
-		while(completedCounter.hasCompleted())
-		{
-			completed(guid,completedCounter);
+
+		while (completedCounter.hasCompleted()) {
+			completed(guid, completedCounter);
 		}
 	}
-    
-    //SAVE
-    	//reduce into inverted index
-    
-    
+
+	// SAVE
+	// reduce into inverted index
+
 }

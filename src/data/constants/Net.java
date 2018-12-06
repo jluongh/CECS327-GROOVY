ac2f45040db61b063f2d413a85bbf3fbc31709e9
Trisha Echual <piechual@gmail.com>
2018-12-06 10:21:18 -0800

    Peer Constants

--- src/data/constants/Net.java
+++ src/data/constants/Net.java
@@ -6,4 +6,17 @@ public class Net {
 
 	public static final String HOST = "localhost"; // temporary
 	public static final int PACKET_SIZE = 4000;
+	
+	/* ---------- p2p ---------- */
+	public static final int TOTAL_PEERS = 9;
+	
+	public static final int ARTIST1_PEER = 0;
+	public static final int ARTIST2_PEER = 1;
+	public static final int ARTIST3_PEER = 2;
+	public static final int ALBUM1_PEER = 3;
+	public static final int ALBUM2_PEER = 4;
+	public static final int ALBUM3_PEER = 5;
+	public static final int SONG1_PEER = 6;
+	public static final int SONG2_PEER = 7;
+	public static final int SONG3_PEER = 8;
 }
